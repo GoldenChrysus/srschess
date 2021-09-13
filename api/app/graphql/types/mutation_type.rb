2ad489dee0b2ee51d80905bc15db9cc9f73dcc5b
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-09-13 17:11:16 +0900

    Add comment

--- api/app/graphql/types/mutation_type.rb
+++ api/app/graphql/types/mutation_type.rb
@@ -2,6 +2,8 @@ module Types
 	class MutationType < Types::BaseObject
 		# /create_user
 		field :create_user, mutation: Types::Mutations::CreateUser
+
+		# /create_repertoire
 		field :create_repertoire, mutation: Types::Mutations::CreateRepertoire
 	end
 end
