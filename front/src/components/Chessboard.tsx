7264ec6423e83e12f8e16ac35d8ce61a2ee3b88b
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-09-18 20:26:46 +0900

    Return to Chessground

--- front/src/components/Chessboard.tsx
+++ front/src/components/Chessboard.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Chessboard as CMChessboard, COLOR, MARKER_TYPE, INPUT_EVENT_TYPE } from "cm-chessboard";
+import Chessground from "react-chessground";
 import Chess, { ChessInstance } from "chess.js";
 
-import "../styles/cm-chessboard.css";
+import "react-chessground/dist/styles/chessground.css";
+import "../styles/chessboard.css";
 
 type ChessType = (fen?: string) => ChessInstance;
 
@@ -22,166 +23,123 @@ class Chessboard extends React.Component<ChessboardProps> {
 	private fen?: string = "start";
 	private pgn?: string = "";
 
-	private board: any;
-
 	constructor(props: ChessboardProps) {
 		super(props);
 
-		this.onMove = this.onMove.bind(this);
+		this.onMove    = this.onMove.bind(this);
+		this.sizeBoard = this.sizeBoard.bind(this);
 	}
 
 	componentDidMount() {
-		this.startup();
-	}
-
-	componentWillUnmount() {
-		this.board?.destroy();
-	}
-
-	componentDidUpdate(prev_props: ChessboardProps) {
-		if (this.props.orientation !== prev_props.orientation && prev_props.orientation && this.props.orientation) {
-			this.board.setOrientation(COLOR[this.props.orientation]);
-		}
-
-		if (this.props.pgn && this.props.pgn !== this.pgn && prev_props.pgn !== this.props.pgn) {
-			this.fen = this.props.fen || "start";
-
-			this.chess.load_pgn(this.props.pgn);
-			this.board.setPosition(this.fen);
-			this.toggleMoveInput();
-		} else if (this.props.fen !== this.fen) {
-			this.fen = this.props.fen || "start";
-
-			this.chess.load(this.fen);
-			this.board.setPosition(this.fen);
-			this.toggleMoveInput();
-		}
+		this.sizeBoard();
+		window.addEventListener("resize", this.sizeBoard);
 	}
 
 	shouldComponentUpdate(next_props: ChessboardProps) {
-		return (
-			this.fen !== next_props.fen ||
-			next_props.orientation !== this.props.orientation ||
-			(
-				next_props.pgn !== this.pgn &&
-				next_props.pgn !== this.props.pgn
-			)
-		);
-	}
+		if (this.props.fen !== next_props.fen) {
+			this.fen = next_props.fen || "start";
+			this.pgn = next_props.pgn || "start";
 
-	startup() {
-		this.board = new CMChessboard(
-			this.board_ref.current,
-			{
-				position    : "start",
-				orientation : (this.props.orientation) ? COLOR[this.props.orientation] : COLOR.white,
-				style       : {
-					moveFromMarker : MARKER_TYPE.square,
-					moveToMarker   : MARKER_TYPE.square
-				},
-				sprite : {
-					url : "/assets/chess/chessboard-sprite-staunty.svg"
-				}
-			}	
-		);
+			this.chess.load(this.fen);
+			this.chess.load_pgn(this.pgn);
+			return true;
+		}
 
-		this.board.enableMoveInput(this.onMove, COLOR.white);
+		if (next_props.pgn !== this.props.pgn) {
+			this.pgn = next_props.pgn || "start";
+
+			this.chess.load_pgn(this.pgn);
+			return true;
+		}
+
+		return (next_props.orientation !== this.props.orientation);
 	}
 
 	render() {
 		return (
-			<div key="chessboard" id="chessboard" ref={this.board_ref}/>
+			<Chessground
+				check={this.checkColor()}
+				turnColor={this.toColor()}
+				movable={this.toDests()}
+				fen={this.props.fen}
+				lastMove={this.lastMove()}
+				onMove={this.onMove}
+				ref={this.board_ref}
+			/>
 		);
 	}
 
-	onMove(event: any) {
-		event.chessboard.removeMarkers(undefined, MARKER_TYPE.dot);
-		event.chessboard.removeMarkers(undefined, MARKER_TYPE.square);
-	
-		switch (event.type) {
-			case INPUT_EVENT_TYPE.moveStart:
-				const moves = this.chess.moves({square: event.square, verbose: true});
-	
-				event.chessboard.addMarker(event.square, MARKER_TYPE.square);
-	
-				for (const move of moves) {
-					event.chessboard.addMarker(move.to, MARKER_TYPE.dot);
-				}
-	
-				return (moves.length > 0);
-	
-			case INPUT_EVENT_TYPE.moveDone:
-				const move = {from: event.squareFrom, to: event.squareTo};
-				const res  = this.chess.move(move);
-	
-				if (!res) {
-					return res;
-				}
-
-				this.fen = this.chess.fen();
-				this.pgn = this.chess.pgn();
-
-				this.toggleMoveInput();
-				event.chessboard.setPosition(this.fen);
-				event.chessboard.removeMarkers(undefined, MARKER_TYPE.frame);
-				this.props.onMove({
-					type  : "move",
-					data  : {
-						fen   : this.fen,
-						pgn   : this.pgn,
-						moves : this.chess.history(),
-					}
-				});	
-				break;
-	
-			default:
-				break;
+	sizeBoard() {
+		if (!this.board_ref.current) {
+			return;
 		}
-	}
-	
-	toggleMoveInput() {
-		const over = this.chess.game_over();
 
-		if (over) {
-			this.board.disableMoveInput();
-		} else {
-			const color = COLOR[(this.chess.turn() === "w") ? "white" : "black"];
+		const board = this.board_ref.current.el;
+		const width = Math.min(window.screen.height, board.closest(".order-1").offsetWidth);
 
-			this.board.enableMoveInput(this.onMove, color);
-		}
+		board.style.width = board.style.height = `${width}px`;
 	}
 
-	handleCheck(next_color: string) {
-		this.board.removeMarkers(undefined, MARKER_TYPE.frame);
+	lastMove() {
+		const history = this.chess.history({ verbose: true });
 
-		if (this.chess.in_check()) {
-			const board = this.chess.board();
+		return (history.length)
+			? [history.at(-1)?.from, history.at(-1)?.to]
+			: null;
+	}
 
-			let king_coord;
+	onMove(orig: any, dest: any) {
+		const res = this.chess.move({
+			from : orig,
+			to   : dest
+		});
 
-			for (const rank_idx in board) {
-				const rank = board[rank_idx];
+		if (!res) {
+			this.chess.undo();
+			return false;
+		}
 
-				if (king_coord) {
-					break;
-				}
+		this.fen = this.chess.fen();
+		this.pgn = this.chess.pgn();
 
-				for (const file_idx in rank) {
-					const file = rank[file_idx];
-
-					if (file?.color === next_color && file?.type === "k") {
-						const rank  = 8 - +rank_idx;
-						const file  = (Array.from({ length: 8 }, (_, i) => String.fromCharCode("a".charCodeAt(0) + i)))[+file_idx];
-						
-						king_coord = file + rank;
-
-						break;
-					}
-				}
+		this.props.onMove({
+			type  : "move",
+			data  : {
+				fen   : this.fen,
+				pgn   : this.pgn,
+				moves : this.chess.history(),
 			}
+		});	
+	}
+	
+	toColor() {
+		return (this.chess.turn() === "w") ? "white" : "black";
+	}
 
-			this.board.addMarker(king_coord, MARKER_TYPE.frame);
-		}
+	checkColor() {
+		return (this.chess.in_check())
+			? this.toColor()
+			: "";
+	}
+
+	toDests() {
+		const dests = new Map();
+
+		this.chess.SQUARES.forEach(s => {
+			const ms = this.chess.moves({
+				square  : s,
+				verbose : true
+			});
+
+			if (ms.length) {
+				dests.set(s, ms.map(m => m.to));
+			}
+		});
+
+		return {
+			free: false,
+			dests: dests
+		};
 	}
 }
 
