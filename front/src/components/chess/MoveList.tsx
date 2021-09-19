c455974d44368df8a01d3f2e479feba72317efe1
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-09-19 17:55:19 +0900

    Fix stretch on ultrawide

--- front/src/components/chess/MoveList.tsx
+++ front/src/components/chess/MoveList.tsx
@@ -13,16 +13,18 @@ class MoveList extends React.PureComponent<MoveListProps> {
 		return (
 			<div key="chess-right-menu" id="chess-right-menu" className="flex-1 order-3 md:order-3 overflow-y-auto">
 				<div key="stockfish" id="stockfish" className="max-w-full">
-					<div key="stockfish-grid" className="grid grid-cols-12 p-2">
-						<div key="stockfish-eval" className="flex justify-center items-center text-center text-2xl font-bold col-span-3">
-							<span>-</span>
-						</div>
-						<div key="stockfish-eval" className="text-left text-xs text-gray-400 col-span-6">
-							<p>Stockfish 14+ <span className="text-green-500 font-medium">NNUE</span></p>
-							<p>Waiting...</p>
-						</div>
-						<div key="stockfish-eval" className="flex justify-end items-center col-span-3">
-							<Switch/>
+					<div className="max-w-full md:max-w-sm">
+						<div key="stockfish-grid" className="grid grid-cols-12 p-2">
+							<div key="stockfish-eval" className="flex justify-center items-center text-center text-2xl font-bold col-span-3">
+								<span>-</span>
+							</div>
+							<div key="stockfish-eval" className="text-left text-xs text-gray-400 col-span-6">
+								<p>Stockfish 14+ <span className="text-green-500 font-medium">NNUE</span></p>
+								<p>Waiting...</p>
+							</div>
+							<div key="stockfish-eval" className="flex justify-end items-center col-span-3">
+								<Switch/>
+							</div>
 						</div>
 					</div>
 				</div>
