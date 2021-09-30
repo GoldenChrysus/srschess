9f72dba1390cae909a6c569538620e713cb93376
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-09-30 20:47:40 +0900

    Implement lesson queue

--- api/app/graphql/types/models/repertoire_type.rb
+++ api/app/graphql/types/models/repertoire_type.rb
@@ -1,11 +1,20 @@
 module Types
 	module Models
 		class RepertoireType < Types::BaseObject
+			class QueueItem < Types::BaseObject
+				field :id, ID, null: false
+				field :move, String, null: false
+				field :uci, String, null: false
+				field :movelist, String, null: false
+			end
+
 			field :id, ID, null: false
 			field :name, String, null: false
 			field :created_at, GraphQL::Types::ISO8601DateTime, null: false
 			field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 			field :side, String, null: false
+			field :lesson_queue, [QueueItem], null: true
+			field :lesson_queue_length, Int, null: true
 
 			# Relationships
 			field :user, Types::Models::UserType, null: true
--- api/app/models/repertoire.rb
+++ api/app/models/repertoire.rb
@@ -1,4 +1,7 @@
 class Repertoire < ApplicationRecord
+	# Attributes
+	attr_accessor :lesson_queue_length
+
 	# Validation
 	validates :user, presence: true
 	validates :name, presence: true
@@ -10,4 +13,67 @@ class Repertoire < ApplicationRecord
 	# Relationships
 	belongs_to :user, required: true
 	has_many :moves, -> { order("move_number ASC, sort ASC") }
+
+	def lesson_queue
+		params = {
+			:repertoire_id => self.id
+		}
+		sql    =
+			"WITH
+				RECURSIVE movetree(id, move, uci, sort, level, movelist, path) AS (
+					SELECT
+						m.id,
+						m.move,
+						m.uci,
+						m.sort,
+						1,
+						ARRAY[m.move],
+						ARRAY[ROW(m.move_number, m.sort)]
+					FROM
+						moves m
+					WHERE
+						m.repertoire_id = :repertoire_id AND
+						m.parent_id IS NULL
+					
+					UNION ALL
+					
+					SELECT
+						m.id,
+						m.move,
+						m.uci,
+						m.sort,
+						level + 1,
+						movelist || m.move,
+						path || ROW(m.move_number, m.sort)
+					FROM
+						moves m,
+						movetree mt
+					WHERE
+						m.parent_id = mt.id
+				)
+			SELECT
+				mt.id,
+				mt.move,
+				mt.uci,
+				array_to_json(mt.movelist) AS movelist
+			FROM
+				movetree mt
+			LEFT JOIN
+				learned_items li
+			ON
+				li.move_id = mt.id
+			WHERE
+				li.id IS NULL
+			ORDER BY
+				-- mt.path ASC; -- depth-first
+				mt.level ASC, mt.sort ASC; -- breadth-first"
+
+		sql = ActiveRecord::Base.sanitize_sql_array([sql, params].flatten)
+		
+		return ActiveRecord::Base.connection.exec_query(sql)
+	end
+
+	def lesson_queue_length
+		return self.lesson_queue.length
+	end
 end
