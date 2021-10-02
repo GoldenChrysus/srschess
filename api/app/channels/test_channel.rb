3aba62666ac716f9a6f5af4bd4a036029531dea7
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-10-03 05:15:17 +0900

    Pubsub testing

--- api/app/channels/test_channel.rb
+++ api/app/channels/test_channel.rb
@@ -1,10 +1,37 @@
+require "redis"
+
 class TestChannel < ApplicationCable::Channel
+	@@red = nil
+	@@subber = []
+
 	def subscribed
-		stream_from "test_channel:" + params[:channelId]
+		channel = "test_channel:" + params[:channelId]
 
-		puts params
-		puts "OK"
+		stream_from channel
 
-		ActionCable.server.broadcast("test_channel:" + params[:channelId], { data: "OK" })
+		@@subber.append(channel)
+
+		@@red = Redis.new(url: "redis://localhost:6379/1")
+
+		@@red.subscribe(channel) do |on|
+			puts "OK1"
+			on.message do |channel, message|
+				puts "onms"
+				puts @@subber
+				ActionCable.server.broadcast(channel, message)
+			end
+			@@red.publish(channel, "OK2")
+		end
+	end
+
+	def receive(message)
+		puts message
+		puts @@subber
+		channel = "test_channel:" + params[:channelId]
+
+		puts channel
+
+		@@red = Redis.new(url: "redis://localhost:6379/1")
+		@@red.publish(channel, message.to_json)
 	end
 end
