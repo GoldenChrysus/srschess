116d4e16734374fafce4900fdf137bb1c0c82836
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-10-11 21:30:04 +0900

    Add localizations

--- front/src/components/ErrorBoundary.tsx
+++ front/src/components/ErrorBoundary.tsx
@@ -1,9 +1,13 @@
 import { message } from "antd";
 import React from "react";
+import i18n from "../i18n";
 
 class ErrorBoundary extends React.Component {
+	static getDerivedStateFromError(error: any) {
+	}
+
 	componentDidCatch(error: any, info: any) {
-		message.error("An unexpected error occurred.");
+		message.error(i18n.t("common:unexpected_error"));
 	}
 
 	render() {
