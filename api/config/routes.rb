fae1821cf37e6a9e29fe749300f2018bb945ae2c
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-10-17 18:38:52 +0900

    Add Rails health route

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	root to: proc { [404, {}, ["Not found."]] }
 
+	get "/health", to: proc { [200, {}, ["healthy\n"]] }
+
 	mount ActionCable.server => "/cable"
 
 	post "/graphql", to: "graphql#execute"
