d7d9ab1dfc2205775aa29ba13c93ee1842c4bf3c
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-12-25 10:46:59 +0900

    Add opening filtering

--- front/src/components/OpeningExplorer.tsx
+++ front/src/components/OpeningExplorer.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { Input } from "antd";
+import React, { ChangeEvent, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { EcoPositionModel, EcoPositionQueryData } from "../lib/types/models/EcoPosition";
 import OpeningVolume from "./opening-explorer/OpeningVolume";
 
@@ -6,37 +8,49 @@ interface OpeningExplorerProps {
 	openings?: EcoPositionQueryData["ecoPositions"]
 }
 
+var processed = false;
+
+const indexed: {[key: string]: Array<EcoPositionModel>} = {
+	A : [],
+	B : [],
+	C : [],
+	D : [],
+	E : []
+};
+
 function OpeningExplorer(props: OpeningExplorerProps) {
-	const indexed: {[key: string]: Array<EcoPositionModel>} = {
-		A : [],
-		B : [],
-		C : [],
-		D : [],
-		E : []
-	};
+	const [ filter, setFilter ] = useState<string | null>(null);
+	const { t } = useTranslation("common");
 
-	for (const opening of props?.openings ?? []) {
-		const index: string = opening.code[0];
+	if (!processed && props?.openings) {
+		for (const opening of props?.openings ?? []) {
+			const index: string = opening.code[0];
 
-		if (!indexed[index]) {
-			continue;
+			if (!indexed[index]) {
+				continue;
+			}
+
+			indexed[index].push(opening);
 		}
 
-		indexed[index].push(opening);
+		processed = true;
+	}
+
+	const onFilter = function(e: ChangeEvent<HTMLInputElement>) {
+		setFilter(e.target.value);
 	}
 
 	const volumes = [];
 
 	for (const volume in indexed) {
-		volumes.push(<OpeningVolume key={"opening-volume-" + volume} volume={volume} openings={indexed[volume]}/>);
+		volumes.push(<OpeningVolume key={"opening-volume-" + volume} volume={volume} openings={indexed[volume]} filter={filter}/>);
 	}
 
 	return (
-		
 		<div className="p-4">
+			<Input addonBefore={t("filter")} onChange={onFilter} allowClear={true}/>
 			{volumes}
 		</div>
-		
 	);
 }
 
--- front/src/components/opening-explorer/OpeningVolume.tsx
+++ front/src/components/opening-explorer/OpeningVolume.tsx
@@ -6,17 +6,33 @@ import OpeningPaginator from "./opening-volume/OpeningPaginator";
 
 interface OpeningVolumeProps {
 	volume: string,
-	openings: Array<EcoPositionModel>
+	openings: Array<EcoPositionModel>,
+	filter: string | null
 }
 
 function OpeningVolume(props: OpeningVolumeProps) {
 	const { t } = useTranslation("chess");
+	let openings: OpeningVolumeProps["openings"] = [];
+
+	if (props.filter?.length) {
+		const filter = props.filter.toLowerCase();
+
+		for (const opening of props.openings) {
+			const search = (opening.code + ": " + opening.name + " " + opening.fen).toLowerCase();
+
+			if (search.indexOf(filter) !== -1) {
+				openings.push(opening);
+			}
+		}
+	} else {
+		openings = props.openings;
+	}
 
 	return (
 		<div className="pb-4">
 			<Divider orientation="left">{t("eco_" + props.volume.toLowerCase())}</Divider>
 			<Spin spinning={!props.openings.length}>
-				<OpeningPaginator openings={props.openings}/>
+				<OpeningPaginator openings={openings}/>
 			</Spin>
 		</div>
 	);
