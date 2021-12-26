9381178ef42c6e99af6d82a22164783e94f5e1b0
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-12-26 14:51:50 +0900

    PWA setup

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "mobx-react";
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import i18n from "./i18n";
 import FirebaseAuth from "./lib/Firebase";
 import AuthState from "./stores/AuthState";
@@ -60,6 +61,11 @@ i18n.setDefaultNamespace("common");
 		document.getElementById("root")
 	);
 })();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
