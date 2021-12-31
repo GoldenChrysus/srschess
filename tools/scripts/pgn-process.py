09f32b98d851e836de34930bd5cef5ea4ff65cff
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-12-31 15:41:49 +0900

    Update process count

--- tools/scripts/pgn-process.py
+++ tools/scripts/pgn-process.py
@@ -359,7 +359,7 @@ if __name__ == "__main__":
 	print("Count: " + str(count))
 
 	processes  = []
-	proc_count = (multiprocessing.cpu_count() - 1) * 8
+	proc_count = multiprocessing.cpu_count() * 2
 	chunk_size = math.ceil(count / proc_count)
 
 	for i in range(proc_count):
