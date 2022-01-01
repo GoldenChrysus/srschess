f7c34b73142dcd8ce0fdd41f7846ab72ba9acc96
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2022-01-01 12:15:07 +0900

    Update build

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 source /etc/profile
 service nginx start
-rails s -d -b 0.0.0.0 -p 3000
+RAILS_ENV=production rake db:migrate
+RAILS_ENV=production rails s -d -b 0.0.0.0 -p 3000
 sleep infinity
