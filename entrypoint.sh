906dd4d448d537ccdb14ec5128215aaf0a6e0dec
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2022-01-01 18:43:06 +0900

    Seed database

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ set -e
 source /etc/profile
 service nginx start
 RAILS_ENV=production rake db:migrate
+RAILS_ENV=production rake db:seed
 RAILS_ENV=production rails s -d -b 0.0.0.0 -p 3000
 sleep infinity
