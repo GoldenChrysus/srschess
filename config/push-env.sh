a96895cabf8fe5329c0909602b8cc4e9a592e1ad
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2022-01-01 14:44:16 +0900

    Add Rails master key

--- config/push-env.sh
+++ config/push-env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cp config/{.env,.env.development,.env.test,.env.production} api/
 cp config/{.env,.env.development,.env.test,.env.production} front/
-cp config/*.json api/config/
+cp config/{*.json,*.key} api/config/
