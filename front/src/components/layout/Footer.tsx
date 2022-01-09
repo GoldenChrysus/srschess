2d5b8e159fda889dc9e73151bac5278c3ac68ed1
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2022-01-09 19:34:07 +0900

    Style changes

--- front/src/components/layout/Footer.tsx
+++ front/src/components/layout/Footer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
 	const { t } = useTranslation(["common", "repertoires", "openings", "database", "premium"]);
@@ -14,8 +16,8 @@ function Footer() {
 
 	return (
 		<div className="bg-gray-800 h-8 absolute -bottom-16 w-full mt-8 flex justify-center items-center text-center">
-			<div className="grid grid-cols-4 md:grid-cols-none md:grid-flow-col gap-x-4 text-gray-500 text-2xs md:text-xs">
-				<div className="col-span-4 md:col-span-1">Copyright &copy; {years.join("-")} Chess HQ, LLC</div>
+			<div className="grid grid-cols-none md:grid-flow-col gap-x-4 text-gray-500 text-2xs md:text-xs">
+				<div className="col-span-5 md:col-span-1">Copyright &copy; {years.join("-")} Chess HQ, LLC</div>
 				<div>
 					<Link to="/repertoires/" className="text-gray-400">{t("repertoires:repertoires")}</Link>
 				</div>
@@ -27,6 +29,9 @@ function Footer() {
 				</div>
 				<div>
 					<Link to="/upgrade/" className="text-gray-400">{t("premium:upgrade")}</Link>
+				</div>
+				<div>
+					<a href="https://twitter.com/ChessHQcom" target="_blank" className="text-gray-400"><FontAwesomeIcon icon={faTwitter}/></a>
 				</div>
 			</div>
 		</div>
