b86fa7f0154968f55d97a07c02265c63e7ad4bdc
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2022-01-18 13:28:33 +0900

    Remove seed

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,5 @@ set -e
 source /etc/profile
 service nginx start
 RAILS_ENV=production rake db:migrate
-RAILS_ENV=production rake db:seed
 RAILS_ENV=production rails s -d -b 0.0.0.0 -p 3000
 sleep infinity
